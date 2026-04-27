b5df2fddbdf61d0f7a229cbc66605c028be73470
Niko <bolotenko.nikita@yandex.ru>
2026-04-27 12:45:39 +0700

    Подчистка кода

--- src/main/java/com/zorold/zorouploader/ModCommands.java
+++ src/main/java/com/zorold/zorouploader/ModCommands.java
@@ -3,7 +3,6 @@ package com.zorold.zorouploader;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.Component;
 
 public class ModCommands {
 
@@ -11,16 +10,13 @@ public class ModCommands {
         dispatcher.register(Commands.literal("zu")
                 .then(Commands.literal("reload")
                         .requires(source -> source.hasPermission(2))
-                        .executes(context -> reloadServer(context.getSource()))
+                        .executes(context -> reloadServer())
                 )
         );
     }
 
-    private static int reloadServer(CommandSourceStack source) {
+    private static int reloadServer() {
         ServerManager.get().reconnect();
-
-        source.sendSystemMessage(Component.literal(ZoroUploader.MODID + " server successfully reloaded."));
-
         return 1;
     }
 }
